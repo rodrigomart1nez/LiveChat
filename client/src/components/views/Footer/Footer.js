1c891b43394b1fe50f07f9cf60478e385fa07cde
rodrigomart1nez <rodrigomartinezgb@outlook.com>
2020-10-11 11:40:11 -0500

    auth logic

--- client/src/components/views/Footer/Footer.js
+++ client/src/components/views/Footer/Footer.js
@@ -1,16 +1,30 @@
 import React from 'react'
 import {Icon} from 'antd';
+import { useSelector } from "react-redux";
 
 function Footer() {
-    return (
-        <div style={{
-            height: '80px', display: 'flex',
-            flexDirection: 'column', alignItems: 'center',
-            justifyContent: 'center', fontSize:'1rem'
-        }}>
-           <p> Type something  <Icon type="smile" /></p>
-        </div>
-    )
+    const user = useSelector(state => state.user)
+    if (user.userData && !user.userData.isAuth) {
+        return (
+            <div style={{
+                height: '80px', display: 'flex',
+                flexDirection: 'column', alignItems: 'center',
+                justifyContent: 'center', fontSize:'1rem'
+            }}>
+               <p> First login to start chatting  <Icon type="smile" /></p>
+            </div>
+        )
+    } else {
+        return (
+            <div style={{
+                height: '80px', display: 'flex',
+                flexDirection: 'column', alignItems: 'center',
+                justifyContent: 'center', fontSize:'1rem'
+            }}>
+               <p> This is a public chat  <Icon type="smile" /></p>
+            </div>
+        )
+    }
 }
 
 export default Footer
--- client/src/components/views/NavBar/Sections/LeftMenu.js
+++ client/src/components/views/NavBar/Sections/LeftMenu.js
@@ -12,7 +12,7 @@ function LeftMenu(props) {
     <Menu.Item key="chat">
       <a href="/chat">Chat</a>
     </Menu.Item>
-    <SubMenu title={<span>Blogs</span>}>
+    {/*<SubMenu title={<span>Blogs</span>}>
       <MenuItemGroup title="Item 1">
         <Menu.Item key="setting:1">Option 1</Menu.Item>
         <Menu.Item key="setting:2">Option 2</Menu.Item>
@@ -21,7 +21,7 @@ function LeftMenu(props) {
         <Menu.Item key="setting:3">Option 3</Menu.Item>
         <Menu.Item key="setting:4">Option 4</Menu.Item>
       </MenuItemGroup>
-    </SubMenu>
+    </SubMenu>*/}
   </Menu>
   )
 }
